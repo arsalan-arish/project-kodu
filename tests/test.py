1166abf6bf5ccb5fb52e0b93a4cf23519e095b51
Arsalan Arish <mail@arsalanarish.com>
2026-02-14 12:16:15 +0500

    Just did some cleaning and organizing

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,10 @@
-import functions as f
+from ..src.core import prompt_response_generator
+
+x = prompt_response_generator("workshop taught a warm-up for help".split(), 1, 10)
+for word in x:
+    print(word, end=" ")
+
+
 
 # print("\n\n            Hi welcome to kodu!             ")
 # n = int(input("Set context length (recommended is 10) --> "))
@@ -9,13 +15,7 @@ import functions as f
 #     while len(prompt) >= 10:
 #         prompt = input("Please enter a prompt under 10 words --> ").split()
 
-#     response = f.prompt_response_generator(prompt, w, n)
+#     response = prompt_response_generator(prompt, w, n)
 #     print("\n\n\n")
 #     for word in response:
 #         print(word, end=" ")
-
-
-# Testing input
-x = f.prompt_response_generator("workshop taught a warm-up for help".split(), 1, 10)
-for word in x:
-    print(word, end=" ")
